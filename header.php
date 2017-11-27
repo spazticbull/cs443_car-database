5993f4af50ba46ff13527012cb448643b2f31118
John Bullington <spazticbull@gmail.com>
2017-11-27 00:15:02 -0600

    Add navbar

--- header.php
+++ header.php
@@ -13,6 +13,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     
     <title><?php echo $title;?></title>
+
+    <style>
+        .brand-logo {
+            margin-left: 20px;
+        }
+    </style>
 </head>
 
 <body>
@@ -22,3 +28,29 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.js"></script>
     <!--Compiled and minified JavaScript-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+    <!--navbar-->
+    <nav>
+        <div class="nav-wrapper">
+            <a href="#!" class="brand-logo">AutoCatz</a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse right"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="service.php"><i class="material-icons left">build</i>Service</a></li>
+                <li><a href="vehicle_add.php"><i class="material-icons left">directions_car</i>Vehicle</a></li>
+                <li><a href="customer_add.php"><i class="material-icons left">tag_faces</i>Customer</a></li>
+                <li><a href="employee_add.php"><i class="material-icons left">work</i>Employee</a></li>
+            </ul>
+            <ul class="side-nav" id="mobile-demo">
+                <li><a href="service.php"><i class="material-icons left">build</i>Service</a></li>
+                <li><a href="vehicle_add.php"><i class="material-icons left">directions_car</i>Vehicle</a></li>
+                <li><a href="customer_add.php"><i class="material-icons left">tag_faces</i>Customer</a></li>
+                <li><a href="employee_add.php"><i class="material-icons left">work</i>Employee</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <script>
+        $(document).ready(function() {
+            $(".button-collapse").sideNav();
+        });
+    </script>
